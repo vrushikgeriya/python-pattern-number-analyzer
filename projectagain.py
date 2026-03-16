78fb70d9622522bf6b27e834d3c970e60793fa20
vrushikgeriya <vrushikgeriya55@gmail.com>
2026-03-16 12:03:29 +0530

    Add files via upload

---
+++ projectagain.py
@@ -0,0 +1,56 @@
+print("\nWelcome to the Pattern Generator and Number Analyzer!")
+while True:
+  
+
+ print("\nSelect an option: ")
+ a = int(input("1.Generate a pattern \n2.Analyze a Range of number \n3.Exit\nEnter your choice: "))
+   
+   
+ match a:
+    case 1:
+        sub = int(input("\nSelect 1 to print left aligned triangle: \nSelect 2 to print a pyramid: \nSelect 3 to print right aligned triangle: \nEnter your choice: "))
+        n = int(input("Enter number of rows: "))
+        match sub:
+           case 1:
+                     for i in range(n+1):
+                      for j in range(i):
+                       print("*",end="")
+                      print()
+           case 2:
+                for i in range(1,n+1):
+                 print(" "*(n-i), end=" ")   
+                 for j in range(i):
+                  print("*", end = " ")
+                 print()
+           case 3:
+              for i in range(1,n+1):
+               for j in range(i,n+1):
+                print(" ", end="")
+               for k in range(1,i+1):
+                print("*", end="")
+               print()
+    case 2:
+      m = int(input("Enter the start of the range: "))
+      n = int(input("Enter the end of the range: "))
+      for i in range(m , n+1):
+       if i%2 == 0:
+        print(f"Number {i} is Even")
+       else:
+        print(f"Number {i} is Odd")
+      s = 0
+      for i in range(m,n+1):
+        s = s+i
+      print(f"Sum of all numbers from {m} to {n} is: {s}")
+      
+    case 3:
+      print("Exiting the program. Goodbye!")
+      break
+
+
+    
+            
+      
+      
+          
+           
+                
